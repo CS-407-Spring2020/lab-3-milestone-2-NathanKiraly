ec6870f7b60d8172f2db7866be31b8f09df0742c
Nathan Kiraly <natekiraly@gmail.com>
2020-02-17 00:36:43 -0600

    Completed all deliverables

--- app/src/main/java/com/example/lab3milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone2/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.lab3milestone2;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button firstFragment, secondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the reference of the buttons
+        firstFragment = (Button) findViewById(R.id.firstFragment);
+        secondFragment = (Button) findViewById(R.id.secondFragment);
+
+        // Perfrom onSetClickListener event on first button
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FirstFragment());
+            }
+        });
+
+        // Perfrom onSetClickListener event on second button
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SecondFragment());
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // Create a fragment manager
+        FragmentManager fm = getFragmentManager();
+
+        // Create a FragmentTransaction to begin a transaction and replace the fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+
+        // Replace the Frame Layout with the new fragment
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+
     }
 }
